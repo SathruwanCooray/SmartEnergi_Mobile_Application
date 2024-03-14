ab63a285f9657d943dd836959d108d85cb344c2b
LIA Perera <induwaraabhiman79@gmail.com>
2024-03-15 02:50:04 +0530

    Created a page called focus modes

---
+++ smartenergi/lib/Pages/FocusMode_Page.dart
@@ -0,0 +1,15 @@
+// date 2024/15/3
+//Focusmodes page
+
+import 'package:flutter/material.dart';
+
+class FocusModePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Focus Mode'),
+      ),
+    );
+  }
+}
