44360bfaf96f2810087d0032e34e23dc6bb93a66
Sathruwan <sathruwancooray@gmail.com>
2024-03-18 11:10:18 +0530

    Fixing the bottom navigation bar

--- smartenergi/lib/Pages/FocusMode_Page.dart
+++ smartenergi/lib/Pages/FocusMode_Page.dart
@@ -1,14 +1,47 @@
-// date 2024/15/3
-//Focusmodes page
-
 import 'package:flutter/material.dart';
+import 'package:smartenergi/Pages/home_screen.dart';
 
-class FocusModePage extends StatelessWidget {
+class FocusModePage extends StatefulWidget {
+  const FocusModePage({Key? key}) : super(key: key);
+
+  @override
+  _FocusModePageState createState() => _FocusModePageState();
+}
+
+class _FocusModePageState extends State<FocusModePage> {
+  int selectedIndex = 1; 
+  void onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+    switch (selectedIndex) {
+      case 0:
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomeScreen(),
+          ),
+        );
+        break;
+
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Focus Mode'),
+        automaticallyImplyLeading: false, // Set to false to hide the back button
+        title: const Text('Focus Mode'),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.layers), label: 'Focus Mode'),
+        ],
+        currentIndex: selectedIndex,
+        selectedItemColor: Colors.black,
+        onTap: onItemTapped,
       ),
     );
   }
